6529b8e7b947a85ab0c78e7b1da17539fc92fb83
houdesen <zh06101019>
2019-10-14 15:52:05 +0800

    build1

---
+++ precache-manifest.8cac76042bf28fabe2210cc22a249ffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4962c06aadbe16965f6e45f7d235cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ee91fb5ae8ebf6f9c6",
+    "url": "/static/css/main.a17942aa.chunk.css"
+  },
+  {
+    "revision": "6391ae8687e6b56aac07",
+    "url": "/static/js/2.06adc54f.chunk.js"
+  },
+  {
+    "revision": "b3ee91fb5ae8ebf6f9c6",
+    "url": "/static/js/main.5c4f1004.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
