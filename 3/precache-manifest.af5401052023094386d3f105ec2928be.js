9a70e03bdf813b0cf649f4a9b7ebee7d8e68d077
houdesen <zh06101019>
2019-11-14 11:48:35 +0800

    3

---
+++ 3/precache-manifest.af5401052023094386d3f105ec2928be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae17752206a1419b9fce15c2694b9d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "980dafbcdba60e3007e2",
+    "url": "static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "980dafbcdba60e3007e2",
+    "url": "static/js/2.d7e202ba.chunk.js"
+  },
+  {
+    "revision": "f5b359de7e3643d3285b",
+    "url": "static/js/main.2eeea0f3.chunk.js"
+  },
+  {
+    "revision": "48372fb5702584896a92",
+    "url": "static/js/runtime-main.c468f56a.js"
+  }
+]);
